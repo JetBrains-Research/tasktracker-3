978ef1297331cc2ca204882206f3edb8fc705fca
mikrise2 <mikrise2@mail.ru>
2023-07-19 14:16:38 +0200

    [ML4SE-68] Content on default pages, more css styles

--- src/main/kotlin/org/jetbrains/research/tasktracker/ui/main/panel/MainPluginPanelFactory.kt
+++ src/main/kotlin/org/jetbrains/research/tasktracker/ui/main/panel/MainPluginPanelFactory.kt
@@ -6,35 +6,40 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.jcef.JBCefApp
 import org.jetbrains.research.tasktracker.ui.main.panel.template.TasksPageTemplate
-import java.awt.GridLayout
+import java.awt.BorderLayout
+import java.awt.FlowLayout
 import java.awt.event.ActionListener
 import javax.swing.JButton
 
 class MainPluginPanelFactory : ToolWindowFactory {
-    private val button = JButton("click me")
+    private val button = JButton("next")
+    private lateinit var mainWindow: MainPluginWindow
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val mainWindow = project.getService(MainWindowService::class.java).mainWindow
+        mainWindow = project.getService(MainWindowService::class.java).mainWindow
         val jComponent = toolWindow.component
-        val panel = JBPanel<JBPanel<*>>(GridLayout(2, 1)).apply { // TODO need we GridLayout?
-            add(mainWindow.jComponent)
-            add(
-                JBPanel<JBPanel<*>>().apply {
-                    add(
-                        button.apply {
-                            addListener {
-                                mainWindow.loadDefaultPage(TasksPageTemplate)
-                                // TODO refactor, add function for button listener and text change
-                            }
-                        }
-                    )
-                }
-            )
+        button.apply {
+            isBorderPainted = false
+            addListener {
+                selectTask()
+            }
         }
-        jComponent.parent.add(panel)
+        val buttonPanel = JBPanel<JBPanel<*>>(FlowLayout()).apply {
+            add(button)
+        }
+        val panel = JBPanel<JBPanel<*>>(BorderLayout()).apply {
+            add(mainWindow.jComponent, BorderLayout.NORTH)
+            add(buttonPanel)
+        }
+        jComponent.add(panel)
     }
 
     override fun isApplicable(project: Project) = super.isApplicable(project) && JBCefApp.isSupported()
+
+    private fun selectTask() {
+        mainWindow.loadDefaultPage(TasksPageTemplate)
+        button.text = "select"
+    }
 
     private fun JButton.addListener(listener: ActionListener) {
         actionListeners.forEach {
