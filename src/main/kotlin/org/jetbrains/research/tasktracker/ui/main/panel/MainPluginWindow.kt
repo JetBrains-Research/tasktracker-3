e81ac946a7b48118289823fc143a05c6781f76f7
mikrise2 <mikrise2@mail.ru>
2023-07-21 01:25:04 +0200

    [ML4SE-68] changed styles, added themes, plugin window listen theme changes

--- src/main/kotlin/org/jetbrains/research/tasktracker/ui/main/panel/MainPluginWindow.kt
+++ src/main/kotlin/org/jetbrains/research/tasktracker/ui/main/panel/MainPluginWindow.kt
@@ -1,5 +1,7 @@
 package org.jetbrains.research.tasktracker.ui.main.panel
 
+import com.intellij.ide.ui.UISettingsListener
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.util.Disposer
 import com.intellij.ui.jcef.JBCefBrowser
 import org.jetbrains.research.tasktracker.ui.main.panel.template.ErrorPageTemplate
@@ -9,17 +11,27 @@ import javax.swing.JComponent
 
 class MainPluginWindow(service: MainWindowService) {
     private val windowBrowser: JBCefBrowser
+    private var currentTemplate: HtmlTemplate
     val jComponent: JComponent
         get() = windowBrowser.component
 
     init {
+        currentTemplate = IndexPageTemplate
         windowBrowser = JBCefBrowser()
         windowBrowser.setErrorPage { errorCode, errorText, failedUrl ->
             ErrorPageTemplate.pageContent(errorCode.code.toString(), errorText, failedUrl)
         }
-        loadDefaultPage(IndexPageTemplate)
+        loadDefaultPage(currentTemplate)
+        val app = ApplicationManager.getApplication().messageBus
+        app.connect().subscribe(
+            UISettingsListener.TOPIC,
+            UISettingsListener {
+                loadDefaultPage(currentTemplate)
+            }
+        )
         Disposer.register(service, windowBrowser)
     }
 
-    fun loadDefaultPage(template: HtmlTemplate) = windowBrowser.loadHTML(template.pageContent())
+    fun loadDefaultPage(template: HtmlTemplate) =
+        windowBrowser.loadHTML(template.pageContent()).also { currentTemplate = template }
 }
--- src/main/kotlin/org/jetbrains/research/tasktracker/ui/main/panel/template/HtmlTemplate.kt
+++ src/main/kotlin/org/jetbrains/research/tasktracker/ui/main/panel/template/HtmlTemplate.kt
@@ -1,5 +1,7 @@
 package org.jetbrains.research.tasktracker.ui.main.panel.template
 
+import com.intellij.util.ui.UIUtil
+
 sealed class HtmlTemplate {
 
     protected abstract val css: CSS
@@ -7,19 +9,29 @@ sealed class HtmlTemplate {
 
     fun pageContent(vararg arguments: String) = pageTemplate().format(*arguments)
 
-    private fun cssContent() =
-        HtmlTemplate::class.java.getResource("css/${css.filename}.css")?.readText()?.replace("%", "%%")
-            ?: error("Cannot find ${css.filename}.css")
+    private fun loadCss(filename: String) =
+        HtmlTemplate::class.java.getResource("css/$filename.css")?.readText()?.replace("%", "%%")
+            ?: error("Cannot find $filename.css")
+
+    private fun cssContent() = loadCss(css.filename)
+
+    private fun baseCss() = loadCss("base")
 
     private fun pageTemplate() = HtmlTemplate::class.java.getResource("template.html")?.readText()?.format(
+        baseCss(),
         cssContent(),
         HtmlTemplate::class.java.getResource("$name.html")?.readText()
             ?: error("Cannot find default page with name $name")
     )
         ?: error("Cannot find page template")
 
-    protected enum class CSS(val filename: String) {
-        DEFAULT("default")
+    protected enum class CSS {
+        DEFAULT {
+            override val filename: String
+                get() = if (UIUtil.isUnderDarcula()) "dark" else "light"
+        };
+
+        abstract val filename: String
     }
 }
 
