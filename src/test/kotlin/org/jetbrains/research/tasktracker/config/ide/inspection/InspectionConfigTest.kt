88c1f84935cb86e686552e3a411fc93a0db4eb14
mikrise2 <mikrise2@mail.ru>
2023-09-12 01:47:55 +0200

    [ML4SE-125] inspection tests

--- src/test/kotlin/org/jetbrains/research/tasktracker/config/ide/inspection/InspectionConfigTest.kt
+++ src/test/kotlin/org/jetbrains/research/tasktracker/config/ide/inspection/InspectionConfigTest.kt
@@ -3,7 +3,6 @@ package org.jetbrains.research.tasktracker.config.ide.inspection
 import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.codeInspection.ex.InspectionProfileImpl
 import com.intellij.profile.codeInspection.InspectionProfileManager
-import com.intellij.profile.codeInspection.ProjectInspectionProfileManager
 import com.intellij.testFramework.LightPlatformTestCase
 import java.io.File
 import java.nio.file.Files
@@ -14,18 +13,78 @@ class InspectionConfigTest : LightPlatformTestCase() {
     override fun setUp() {
         super.setUp()
         CodeInsightSettings.getInstance().AUTO_POPUP_COMPLETION_LOOKUP = true
+        setupProjectProfile()
     }
 
     fun testAllInspections() {
-        val profile = setupProjectProfile()
-        val configFile = loadConfig("all")
-        applyConfig(configFile, profile)
+        val profile = getProfile("all")
         assert(profile.allTools.all { it.isEnabled }) {
             "all available tools must be enabled, the list of disabled tools: ${profile.getAllDisabledToolIds()}"
         }
     }
 
+    fun testNoneInspections() {
+        val profile = getProfile("none")
+        assert(profile.allTools.all { !it.isEnabled }) {
+            "all available tools must be disabled, the list of disabled tools: ${profile.getAllEnabledToolIds()}"
+        }
+    }
+
+    fun testDefaultInspections() {
+        val profile = getProfile("default")
+        val enabledToolsSize = profile.allTools.count { it.isEnabled }
+        assert(enabledToolsSize == defaultEnabledInspectionSize) {
+            "default enabled tools size is $defaultEnabledInspectionSize, but $enabledToolsSize has been found"
+        }
+    }
+
+    fun testEnableSelectedInspections() {
+        val profile = getProfile("enableSelected")
+        val enabledTools = profile.getAllEnabledToolIds()
+        assert(enabledTools.size == enableSelectedTools.size) {
+            "enable selected tools size is ${enableSelectedTools.size}, but ${enabledTools.size} has been found"
+        }
+        assert(enabledTools.containsAll(enableSelectedTools)) {
+            "enable selected tools must be $enableSelectedTools, but $enabledTools have been found"
+        }
+    }
+
+    fun testDisableSelectedInspections() {
+        val profile = getProfile("disableSelected")
+        val enabledTools = profile.getAllEnabledToolIds()
+        assert(enabledTools.size == disableSelectedEnabledInspectionSize) {
+            buildString {
+                append("disable selected tools size is $disableSelectedEnabledInspectionSize")
+                append(", but ${enabledTools.size} has been found")
+            }
+        }
+        assert(disableSelectedTools.all { !enabledTools.contains(it) }) {
+            "disable selected tools must not contains $disableSelectedTools, but $enabledTools have been found"
+        }
+    }
+
+    fun testAddSelectedInspections() {
+        val profile = getProfile("addSelected")
+        val enabledTools = profile.getAllEnabledToolIds()
+        println(profile.getAllDisabledToolIds())
+        assert(enabledTools.size == addSelectedEnabledInspectionSize) {
+            "add selected tools size is $addSelectedEnabledInspectionSize, but ${enabledTools.size} has been found"
+        }
+        assert(enabledTools.containsAll(addSelectedTools)) {
+            "add selected tools must contains $addSelectedTools, but ${enabledTools.size} has been found"
+        }
+    }
+
+    private fun getProfile(configName: String): InspectionProfileImpl {
+        val profile = InspectionProfileManager.getInstance().currentProfile
+        val configFile = loadConfig(configName)
+        applyConfig(configFile, profile)
+        return profile
+    }
+
     private fun InspectionProfileImpl.getAllDisabledToolIds() = allTools.filter { !it.isEnabled }.map { it.tool.id }
+
+    private fun InspectionProfileImpl.getAllEnabledToolIds() = allTools.filter { it.isEnabled }.map { it.tool.id }
 
     private fun applyConfig(configFile: File, profile: InspectionProfileImpl) {
         val inspectionConfig = InspectionConfig.buildConfig(configFile)
@@ -35,7 +94,7 @@ class InspectionConfigTest : LightPlatformTestCase() {
     private fun loadConfig(name: String): File {
         val configText = InspectionConfigTest::class.java
             .getResource("$name.yaml")?.readText()
-            ?: error("Cannot find codeTrackingConfig.yaml!")
+            ?: error("Cannot find `$name.yaml`!")
         val file = Files.createTempFile(name, ".yaml")
         file.writeText(configText)
         return file.toFile()
@@ -46,9 +105,31 @@ class InspectionConfigTest : LightPlatformTestCase() {
      */
     private fun setupProjectProfile(): InspectionProfileImpl {
         InspectionProfileImpl.INIT_INSPECTIONS = true
-        val inspectionProfileManager = ProjectInspectionProfileManager.getInstance(project)
         val profile = InspectionProfileManager.getInstance().currentProfile
-        inspectionProfileManager.currentProfile.copyFrom(profile)
-        return InspectionProfileManager.getInstance().currentProfile
+        profile.resetInspections()
+        return profile
+    }
+
+    private fun InspectionProfileImpl.resetInspections() {
+        defaultTools.apply {
+            if (isEmpty()) {
+                addAll(getAllEnabledToolIds())
+            } else {
+                disableAllTools(project)
+                forEach {
+                    enableTool(it, project)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val defaultEnabledInspectionSize = 56
+        const val disableSelectedEnabledInspectionSize = 54
+        const val addSelectedEnabledInspectionSize = 58
+        val enableSelectedTools = listOf("LongLine", "TodoComment")
+        val disableSelectedTools = listOf("CheckTagEmptyBody", "RegExpRedundantEscape")
+        val addSelectedTools = listOf("EmptyDirectory", "ProblematicWhitespace")
+        val defaultTools = mutableListOf<String>()
     }
 }
