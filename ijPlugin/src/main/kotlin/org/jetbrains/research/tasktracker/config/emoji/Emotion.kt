44433e1149a2c1045ee82541d04dee345a8fefb6
mikrise2 <mikrise2@mail.ru>
2023-10-11 22:18:18 +0200

    [ML4SE-137] removed ui items for emotion detection

--- ijPlugin/src/main/kotlin/org/jetbrains/research/tasktracker/config/emoji/Emotion.kt
+++ ijPlugin/src/main/kotlin/org/jetbrains/research/tasktracker/config/emoji/Emotion.kt
@@ -1,29 +1,9 @@
 package org.jetbrains.research.tasktracker.config.emoji
 
-import com.intellij.openapi.util.IconLoader
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
-import javax.swing.Icon
 
 @Serializable
 open class Emotion(
     val modelPosition: Int,
     val name: String,
-    private val affirmDescriptions: List<String>,
-    private val adviceDescriptions: List<String> = emptyList()
-) {
-    protected open val iconName: String? = null
-    protected open val modalWindowIconName: String? = null
-
-    @Transient
-    val icon: Icon? = iconName?.let { IconLoader.getIcon(it, Emotion::class.java) }
-
-    @Transient
-    val modalWindowIcon: Icon? = modalWindowIconName?.let { IconLoader.getIcon(it, Emotion::class.java) }
-
-    val randomAffirmDescription
-        get() = affirmDescriptions.random()
-
-    val randomAdviceDescription
-        get() = adviceDescriptions.random()
-}
+)
--- ijPlugin/src/main/kotlin/org/jetbrains/research/tasktracker/tracking/webcam/WebCamTracker.kt
+++ ijPlugin/src/main/kotlin/org/jetbrains/research/tasktracker/tracking/webcam/WebCamTracker.kt
@@ -1,10 +1,7 @@
 package org.jetbrains.research.tasktracker.tracking.webcam
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import kotlinx.coroutines.runBlocking
-import org.jetbrains.research.tasktracker.actions.tracking.NotificationIcons
-import org.jetbrains.research.tasktracker.actions.tracking.NotificationWrapper
 import org.jetbrains.research.tasktracker.modelInference.EmoPredictor
 import org.jetbrains.research.tasktracker.tracking.BaseTracker
 import org.jetbrains.research.tasktracker.tracking.logger.WebCamLogger
@@ -26,26 +23,11 @@ class WebCamTracker(project: Project, private val emoPredictor: EmoPredictor) : 
                     runBlocking {
                         it.guessEmotionAndLog(emoPredictor, trackerLogger)
                     }
-                    if (photosMade == PHOTOS_MADE_BEFORE_NOTIFICATION) {
-                        showNotification()
-                    }
                 }
             },
             TIME_TO_PHOTO_DELAY,
             TIME_TO_PHOTO_DELAY
         )
-    }
-
-    private fun showNotification() {
-        ApplicationManager.getApplication().invokeAndWait {
-            ApplicationManager.getApplication().runReadAction {
-                NotificationWrapper(
-                    NotificationIcons.feedbackNotificationIcon,
-                    NotificationWrapper.FEEDBACK_TEXT,
-                    null
-                ).show()
-            }
-        }
     }
 
     override fun stopTracking() {
@@ -55,7 +37,5 @@ class WebCamTracker(project: Project, private val emoPredictor: EmoPredictor) : 
     companion object {
         // 5 sec
         private const val TIME_TO_PHOTO_DELAY = 5000L
-
-        private const val PHOTOS_MADE_BEFORE_NOTIFICATION: Int = (60 * 60 / (TIME_TO_PHOTO_DELAY / 1000)).toInt()
     }
 }
