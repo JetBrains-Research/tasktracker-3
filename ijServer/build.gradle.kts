592736017981fbdd5ddfdf1bcb6c5b5631e10b07
mikrise2 <mikrise2@mail.ru>
2023-10-24 16:47:11 +0200

    [ML4SE-184] Fixed gradle structure

--- ijServer/build.gradle.kts
+++ ijServer/build.gradle.kts
@@ -1,16 +1,10 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
-val postgresql_driver_version: String by project
-val exposed_version: String by project
-
-plugins {
-    id("io.ktor.plugin") version "2.3.4"
-}
-
 group = rootProject.group
 version = rootProject.version
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id(libs.plugins.ktor.get().pluginId) version libs.versions.ktor.get()
+}
 
 application {
     mainClass.set("org.jetbrains.research.tasktracker.ApplicationKt")
@@ -19,18 +13,13 @@ application {
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.postgresql:postgresql:$postgresql_driver_version")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(rootProject.libs.ktor.server.core)
+    implementation(rootProject.libs.ktor.server.netty)
+    implementation(rootProject.libs.logback)
+    implementation(rootProject.libs.postgres)
+    implementation(rootProject.libs.exposed.core)
+    implementation(rootProject.libs.exposed.dao)
+    implementation(rootProject.libs.exposed.jdbc)
+    testImplementation(rootProject.libs.ktor.server.tests)
 }
