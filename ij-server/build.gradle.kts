3044832f398275659739c00397ee85bc99756f05
mikrise2 <mikrise2@mail.ru>
2023-11-12 22:24:25 +0100

    some changes

--- ij-server/build.gradle.kts
+++ ij-server/build.gradle.kts
@@ -4,10 +4,12 @@ version = rootProject.version
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id(libs.plugins.ktor.get().pluginId) version libs.versions.ktor.get()
+    id("com.google.cloud.tools.jib") version "3.3.1"
 }
 
 application {
     mainClass.set("org.jetbrains.research.tasktracker.ApplicationKt")
+
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
@@ -24,4 +26,24 @@ dependencies {
     implementation(rootProject.libs.exposed.time)
     testImplementation(rootProject.libs.ktor.server.tests)
     testImplementation(rootProject.libs.h2)
+}
+
+jib {
+    container {
+        ports = listOf("8080")
+        mainClass = "org.jetbrains.research.tasktracker.ApplicationKt"
+
+        jvmFlags = listOf(
+            "-server",
+            "-Djava.awt.headless=true",
+            "-XX:+UnlockExperimentalVMOptions",
+            "-XX:+UseCGroupMemoryLimitForHeap",
+            "-XX:InitialRAMFraction=2",
+            "-XX:MinRAMFraction=2",
+            "-XX:MaxRAMFraction=2",
+            "-XX:+UseG1GC",
+            "-XX:MaxGCPauseMillis=100",
+            "-XX:+UseStringDeduplication"
+        )
+    }
 }
