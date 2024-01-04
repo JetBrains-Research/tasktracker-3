e8fbf0e0932cd87e949280bc6bc69298abd5ed34
mikrise2 <mikrise2@mail.ru>
2024-01-04 14:13:52 +0100

    test2

--- ij-plugin/src/main/kotlin/org/jetbrains/research/tasktracker/activities/InitActivity.kt
+++ ij-plugin/src/main/kotlin/org/jetbrains/research/tasktracker/activities/InitActivity.kt
@@ -8,11 +8,11 @@ import org.jetbrains.research.tasktracker.TaskTrackerPlugin
 import org.jetbrains.research.tasktracker.config.MainTaskTrackerConfig
 
 // Put into plugin.xml
-class InitActivity : ProjectActivity {
-    private val logger: Logger = Logger.getInstance(javaClass)
-
-    override suspend fun execute(project: Project) {
-        logger.info("${MainTaskTrackerConfig.PLUGIN_NAME}: startup activity")
-        TaskTrackerPlugin.initPlugin()
-    }
-}
+//class InitActivity : ProjectActivity {
+//    private val logger: Logger = Logger.getInstance(javaClass)
+//
+//    override suspend fun execute(project: Project) {
+//        logger.info("${MainTaskTrackerConfig.PLUGIN_NAME}: startup activity")
+//        TaskTrackerPlugin.initPlugin()
+//    }
+//}
