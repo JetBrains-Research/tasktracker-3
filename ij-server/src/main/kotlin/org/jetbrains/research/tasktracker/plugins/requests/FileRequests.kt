b57ccd132aca268875518646c609757c09e228b3
mikrise2 <56760252+mikrise2@users.noreply.github.com>
2024-03-14 14:46:01 +0100

    [ML4SE-524] Run saving to the database in the background (#101)

--- ij-server/src/main/kotlin/org/jetbrains/research/tasktracker/plugins/requests/FileRequests.kt
+++ ij-server/src/main/kotlin/org/jetbrains/research/tasktracker/plugins/requests/FileRequests.kt
@@ -6,6 +6,8 @@ import io.ktor.server.plugins.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.jetbrains.research.tasktracker.util.createLogFile
 import org.jetbrains.research.tasktracker.util.logFile.parseLogFile
@@ -20,10 +22,10 @@ fun Routing.uploadLogFile() {
             val logFileType = parameters["logFileType"] ?: DEFAULT_FOLDER
             val researchId = parameters.getOrFail<Int>("id")
             val logFile = createLogFile(logFileType, researchId)
-            call.respond(HttpStatusCode.OK)
-            launch {
+            CoroutineScope(Job()).launch {
                 logFile.parseLogFile(logFileType, researchId)
             }
+            call.respond(HttpStatusCode.OK)
         } catch (e: MissingRequestParameterException) {
             call.respond(HttpStatusCode.BadRequest, e.localizedMessage)
         } catch (e: ParameterConversionException) {
