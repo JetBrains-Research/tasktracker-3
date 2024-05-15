9a9cc5a71bbc2e46271eb5d20b89f827d498e776
mikrise2 <mikrise2@mail.ru>
2024-05-15 19:27:25 +0200

    [ML4SE-666] created code fragments directory and dataframe

--- processing/src/main/kotlin/org/jetbrains/research/tasktracker/Main.kt
+++ processing/src/main/kotlin/org/jetbrains/research/tasktracker/Main.kt
@@ -5,13 +5,22 @@
 
 package org.jetbrains.research.tasktracker
 
+import kotlinx.datetime.LocalDateTime
 import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.DataRow
 import org.jetbrains.kotlinx.dataframe.annotations.ImportDataSchema
 import org.jetbrains.kotlinx.dataframe.api.*
 import org.jetbrains.kotlinx.dataframe.io.readCSV
+import org.jetbrains.kotlinx.dataframe.size
+import java.io.File
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
+import kotlin.io.path.createDirectories
+import kotlin.io.path.createDirectory
+import kotlin.io.path.exists
 
+
+var counter = 1
 
 //TODO
 private var activityData =
@@ -38,12 +47,65 @@ fun getAllActivityData() =
     activityData.concat(fileEditorData).concat(toolWindowData).dropNA("date")
         .update("date")
         .with { OffsetDateTime.parse(it.toString(), dateFormatter).toString() }
-        .join(researches){ "research_id" match "id" }
+        .join(researches) { "research_id" match "id" }
         .convert("date")
-        .toLocalDateTime().sortBy("date")
+        .toLocalDateTime()
+        .sortBy("date")
 
+fun getAllDocumentData() =
+    documentData.dropNA("date")
+        .update("date")
+        .with { OffsetDateTime.parse(it.toString(), dateFormatter).toString() }
+        .join(researches) { "research_id" match "id" }
+        .convert("date")
+        .toLocalDateTime()
+
+data class FilesDirectory(val date: LocalDateTime, val researchId: String, val directory: Int)
+
+val directory = File("test-processing")
+
+fun writeToFiles(acc: Collection<DataRow<*>>) = acc.forEach { fileRow ->
+    val fileName = fileRow["filename"].toString().split("/", "\\").last()
+    val dir = directory.toPath().resolve(counter.toString())
+    if (!dir.exists()) {
+        dir.createDirectories()
+    }
+    dir.resolve(fileName).toFile().let {
+        if (!it.exists()) {
+            it.createNewFile()
+            it.writeText(fileRow["fragment"].toString())
+        }
+    }
+}
+
+fun getCodeDataFrame(): DataFrame<*> {
+    val files = mutableSetOf<FilesDirectory>()
+    val allDocuments = getAllDocumentData()
+    allDocuments.groupBy("research_id").forEach {
+        val acc = mutableSetOf<DataRow<*>>()
+        val sortBy = it.group.sortBy("date")
+        sortBy.rows().forEach { row ->
+            if (acc.find { it["filename"] == row["filename"] } == null)
+                acc.add(row)
+        }
+        val date = acc.minOf { it.get("date") as LocalDateTime }
+        writeToFiles(acc)
+        files.add(FilesDirectory(date, it.key.toString(), counter++))
+        sortBy.rows().forEach { row ->
+            if (row !in acc) {
+                acc.removeIf { it["filename"] == row["filename"] }
+                acc.add(row)
+                //TODO write to files
+                writeToFiles(acc)
+                files.add(FilesDirectory(row["date"] as LocalDateTime, it.key.toString(), counter++))
+            }
+        }
+    }
+    return dataFrameOf(
+        listOf("date", "research_id", "directory"),
+        files.flatMap { listOf(it.date, it.researchId, it.directory) })
+}
 
 fun main() {
-    val allActivity = getAllActivityData()
-    allActivity.print()
+
 }
